33f07226dd70e5e58912d938fba82e3e6c7ca63a
Markec0 <ilija.markovic595@gmail.com>
2024-03-20 17:30:57 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let userInput;
+
+document.getElementById("sbutton").onclick = function () {
+  userInput = document.getElementById("cname").value;
+  console.log(userInput);
+  userInput = document.getElementById("sname").value;
+  console.log(userInput);
+  userInput = document.getElementById("ctyname").value;
+  console.log(userInput);
+  userInput = document.getElementById("pname").value;
+  console.log(userInput);
+  userInput = document.getElementById("cntname").value;
+  console.log(userInput);
+  userInput = document.getElementById("ename").value;
+  console.log(userInput);
+  userInput = document.getElementById("tlkname").value;
+  console.log(userInput);
+};
